cb45eda6f798675527f943540691146652b18c6a
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-03-21 10:39:59 +0100

    creating base folders

--- EventiaWebapp/Program.cs
+++ EventiaWebapp/Program.cs
@@ -1,5 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.MapGet("/", () => "Hello World!");
 
