4b5cb620e36390d372b65f926f5cb43de923cb98
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-04-26 14:15:24 +0200

    Refactor AdminController

--- EventiaWebapp/Controllers/AdminController.cs
+++ EventiaWebapp/Controllers/AdminController.cs
@@ -1,7 +1,6 @@
 ﻿using EventiaWebapp.Models;
 using EventiaWebapp.Service;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventiaWebapp.Controllers
@@ -9,49 +8,27 @@ namespace EventiaWebapp.Controllers
     [Authorize(Roles = MyRole.Admin)]
     public class AdminController : Controller
     {
-        private readonly UserManager<User> _userManager;
-        private readonly Database _databaseService;
-        private readonly SignInManager<User> _signInManager;
+        private readonly AdminsHandler _adminsHandler;
 
-        public AdminController(UserManager<User> userManager, Database databaseService, SignInManager<User> signInManager)
+        public AdminController(AdminsHandler adminsHandler)
         {
-            _userManager = userManager;
-            _databaseService = databaseService;
-            _signInManager = signInManager;
+            _adminsHandler = adminsHandler;
         }
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            var users = _userManager.Users.ToList();
+            var users = await _adminsHandler.GetUsers();
             return View(users);
         }
         public async Task<ActionResult> EditUser(string id)
         {
-            var OrganizerUser = await _userManager.FindByIdAsync(id);
-
-            return View(OrganizerUser);
+            var organizerUser = await _adminsHandler.GetUserById(id);
+            return View(organizerUser);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> EditUser(User user)
         {
-            var organizerUser = await _userManager.FindByIdAsync(user.Id);
-
-            if (organizerUser == null) return NotFound();
-            if (user.isOrganizer == true)
-            {
-                organizerUser.isOrganizer = true;
-                await _userManager.AddToRoleAsync(organizerUser, MyRole.Organizer);
-                await _userManager.UpdateAsync(organizerUser);
-                return RedirectToAction(nameof(Index));
-            }
-            else if (user.isOrganizer == false)
-            {
-                organizerUser.isOrganizer = false;
-                await _userManager.AddToRoleAsync(organizerUser, MyRole.Attendee);
-                await _userManager.RemoveFromRoleAsync(organizerUser, MyRole.Organizer);
-                await _userManager.UpdateAsync(organizerUser);
-                return RedirectToAction(nameof(Index));
-            }
+            await _adminsHandler.ChangeRole(user);
             return RedirectToAction(nameof(Index));
         }
         public ViewResult DbReset()
@@ -62,8 +39,7 @@ namespace EventiaWebapp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<RedirectToActionResult> DbResetConfirmation()
         {
-            await _databaseService.RecreateAndSeed();
-            await _signInManager.SignOutAsync();
+            await _adminsHandler.ResetDb();
             return RedirectToAction(nameof(Index));
         }
     }
